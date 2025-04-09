7ec2742288246dbf7442a690191b459b26119600
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2025-04-08 21:20:00 -0500

    adding session capability

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 // pull in the express server framework
 const express = require("express");
 
+// pull in session management
+const session = require("express-session");
+
+// retrieve database connection
+const sequelize = require('./config/connection');
+
+// create ability to store sessions in the database
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// enable session support in app
+const sess = {
+    // sign the session id cookie
+    secret: process.env.SECRET,
+    cookie: {
+        // 900000 milliseconds is 15 minutes
+        maxAge: 900000,
+    },
+    // prevent session from being saved again to the session store if nothing has changed
+    resave: false,
+    // only save sessions when something has been added to it
+    saveUninitialized: false,
+    // instead of keeping session data in memory (which resets when the server restarts), store them persistently in the database
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+
 // pulls in all routes created in the conrollers folder
 // (it looks at the index.js file in the controllers folder)
 const routes = require('./controllers');
@@ -10,6 +38,9 @@ const app = express();
 
 // use port 3001 if environment variables are not found
 const PORT = process.env.PORT || 3001;
+
+// mount session handling middleware to the app
+app.use(session(sess));
 
 // format calls and responses to and from server in json form
 app.use(express.json());
@@ -24,5 +55,9 @@ app.use((req, res) => {
 
 // start the server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+
+});
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
 });
