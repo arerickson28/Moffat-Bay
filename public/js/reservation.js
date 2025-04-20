b4d0924e7d94a5fe633628a6cef585227bb6701c
Deena <deenalinehan@gmail.com>
2025-04-19 22:05:47 -0500

    Final Updates

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -1,5 +1,33 @@
 console.log("hellooooooooo");
 
+let guestCountRetrieved;
+let bedSelectionRetrieved;
+let dateRange;
+let checkInDate;
+let checkOutDate;
+let confirmationNumber;
+
+let confirmResBtn = document.getElementById("confirmRes");
+
+function repopWithConf() {
+    let modalContent = document.getElementsByClassName("modal-content") [0];
+    const keepId = "closeModalBtn";
+    for (let i = modalContent.children.length -1; i >= 0; i--) {
+        const child = modalContent.children[i];
+        if (child.id !== keepId) {
+            modalContent.removeChild(child);
+        }
+    }
+    let resCreatedMsg = document.createElement("h2")
+    resCreatedMsg.innerHTML = "Reservation Created!";
+
+    let confNumber = document.createElement("p")
+    confNumber.innerHTML = `Your confirmation number is: ${confirmationNumber}`
+
+    modalContent.appendChild(resCreatedMsg);
+    modalContent.appendChild(confNumber)
+}
+
 // if the confirmation div has been revealed from a previous reservation, hide it
 // const confimrationDiv = document.getElementById("confirmationDiv")
 // confimrationDiv.classList.add("hidden")
@@ -25,56 +53,15 @@ function generateConfirmationNumber() {
 let submitButton = document.getElementById("submitButton");
 submitButton.addEventListener('click', async () => {
 
-    // gather data for POST request body
-    const guestCountRetrieved = document.getElementById("guestCount").value;
-    const bedSelectionRetrieved = document.getElementById("roomSelection").value;
-    const dateRange = document.getElementById("dateselect").value;
-    const [checkInDate, checkOutDate] = dateRange.split(" - ");
-    const confirmationNumber = generateConfirmationNumber();
-
-    try {
-        let response = await fetch("/api/session/getSession", {
-            method: 'GET'
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const sessionData = await response.json();
-
-        let preparedBody = {
-            "guestCount": guestCountRetrieved,
-            "userId": sessionData.userId,
-            "roomId": bedSelectionRetrieved,
-            "checkInDate": checkInDate,
-            "checkOutDate": checkOutDate,
-            "confirmationNumber": confirmationNumber
-        }
-
-        const response2 = await fetch('/api/reservations/newRes', {
-            method: 'POST',
-            body: JSON.stringify(preparedBody),
-            headers: { 'Content-Type': 'application/json' }
-        })
-
-        if (!response2.ok) throw new Error('Second fetch failed');
-        const data2 = await response2.json();
-        console.log(data2)
-
-        // clear input fields
-        document.getElementById('dateselect').value = '';
-        document.getElementById('guestCount').value = '';
-        document.getElementById('roomSelection').selectedIndex = 0;
-
-        // clear calendar selection
-        if (picker) {
-            picker.clear();
-        }
+    guestCountRetrieved = document.getElementById("guestCount").value;
+    bedSelectionRetrieved = document.getElementById("roomSelection").value;
+    dateRange = document.getElementById("dateselect").value;
+    [checkInDate, checkOutDate] = dateRange.split(" - ");
+    confirmationNumber = generateConfirmationNumber();
 
        // show modal and set confirmation code
         // document.getElementById("confirmationCode").textContent = confirmationNumber;
-        document.getElementById("modalConfirmationNumber").textContent = confirmationNumber;
+        // document.getElementById("modalConfirmationNumber").textContent = confirmationNumber;
         document.getElementById("modalCheckIn").textContent = checkInDate;
         document.getElementById("modalCheckOut").textContent = checkOutDate;
         document.getElementById("modalGuests").textContent = guestCountRetrieved;
@@ -83,10 +70,6 @@ submitButton.addEventListener('click', async () => {
         document.getElementById("modalRoomType").textContent = roomOption ? roomOption.textContent : "Room not found";
 
         document.getElementById("confirmationModal").style.display = "block";
-        console.log(preparedBody);
-    } catch (error) {
-        console.error("Error fetching session data:", error);
-    }
 });
 
 
@@ -160,7 +143,73 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 5000);
 });
 
-// Close modal on 'X' click
-document.getElementById("closeModal").addEventListener("click", () => {
+function closeModal() {
     document.getElementById("confirmationModal").style.display = "none";
-  });
+    location.reload();
+  }
+
+// Close modal on 'X' click
+document.getElementById("closeModalBtn").addEventListener("click", closeModal);
+
+  document.getElementById("cancelRes").addEventListener("click", closeModal);
+
+ async function sendResToBackend() {
+        // // gather data for POST request body
+        // guestCountRetrieved = document.getElementById("guestCount").value;
+        // bedSelectionRetrieved = document.getElementById("roomSelection").value;
+        // dateRange = document.getElementById("dateselect").value;
+        // [checkInDate, checkOutDate] = dateRange.split(" - ");
+        // confirmationNumber = generateConfirmationNumber();
+    
+        try {
+            let response = await fetch("/api/session/getSession", {
+                method: 'GET'
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+    
+            const sessionData = await response.json();
+            console.log(sessionData)
+    
+            let preparedBody = {
+                "guestCount": guestCountRetrieved,
+                "userId": sessionData.userId,
+                "roomId": bedSelectionRetrieved,
+                "checkInDate": checkInDate,
+                "checkOutDate": checkOutDate,
+                "confirmationNumber": confirmationNumber
+            }
+
+            console.log(preparedBody)
+    
+            const response2 = await fetch('/api/reservations/newRes', {
+                method: 'POST',
+                body: JSON.stringify(preparedBody),
+                headers: { 'Content-Type': 'application/json' }
+            })
+    
+            if (!response2.ok) throw new Error('Second fetch failed');
+            const data2 = await response2.json();
+            console.log(data2)
+    
+            // clear input fields
+            document.getElementById('dateselect').value = '';
+            document.getElementById('guestCount').value = '';
+            document.getElementById('roomSelection').selectedIndex = 0;
+    
+            // clear calendar selection
+            if (picker) {
+                picker.clear();
+            }
+        } catch (error) {
+            console.error("Error fetching session data:", error);
+        }
+  }
+
+  confirmResBtn.addEventListener('click', () => {
+
+  sendResToBackend()
+  repopWithConf()
+})
